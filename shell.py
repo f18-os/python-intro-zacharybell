e81525c474bd27198002e80216abf9345f04d4f3
Zachary Bell <zacharybell@live.com>
2018-09-19 14:08:58 -0600

    Implement pipes and major refactor

--- shell.py
+++ shell.py
@@ -1,47 +1,98 @@
-import os, sys
+#! /usr/bin/env python3
+
+import os, sys, webbrowser
 
 
-user_input = ''
+def execute_cmd_sq(cmd_sq):
+
+    if '|' in cmd_sq:
+        
+        p1, p2 = map(str.strip, cmd_sq.split('|', 1))
+
+        # get fds for pipe
+        pr, pw = os.pipe()
+        os.set_inheritable(pr, True)
+        os.set_inheritable(pw, True)
+        
+        # create fork for execution
+        rc = os.fork()
+
+        if rc < 0:
+            raise Exception('fork failed')
+        
+        # child handles execution
+        elif rc == 0:
+            
+            # set stdout to pw
+            os.dup2(pw, 1)
+            
+            os.close(pw)
+            os.close(pr)
+
+            # separate program from flags, ect.
+            p1 = p1.split()
+            os.execvp(p1[0], p1)
+        
+        # parent reassigns input and continues
+        else:
+            
+            # wait for child to finish
+            os.wait()
+
+            # set stdin to pr
+            os.dup2(pr, 0)
+
+            os.close(pw)
+            os.close(pr)
+
+            execute_cmd_sq(p2)
+
+    elif '<' in cmd_sq:
+        
+        # p2 is the file that is used for input into p1
+        p1, p2 = map(str.strip, cmd_sq.rsplit('<', 1))
+
+        os.close(0)
+        sys.stdin = open(p2, 'r')
+        os.set_inheritable(sys.stdin.fileno(), True)
+
+        execute_cmd_sq(p1)
+
+    elif '>' in cmd_sq:
+
+        # p2 is the file that is used for output from p1
+        p1, p2 = map(str.strip, cmd_sq.rsplit('>', 1))
+
+        os.close(1)
+        sys.stdout = open(p2, 'w')
+        os.set_inheritable(sys.stdout.fileno(), True)
+
+        execute_cmd_sq(p1)
+
+    # command seq with no pipes or redirection
+    elif cmd_sq:
+
+        cmd_sq = cmd_sq.split()
+        os.execvp(cmd_sq[0], cmd_sq)
+
+    else:
+        return webbrowser.open('https://www.warnerbros.com/archive/spacejam/movie/jam.htm')
+
+
+raw = ''
 while(True):
-    user_input = input('> ')
-    if (user_input == 'exit'):
-        break
     
-    child = os.fork()
-        
-    if (child < 0):
-        raise Exception('Failed to create new process!')
-    if (child == 0):
+    raw = input('$ ')
+    
+    if raw == 'exit':
+        break
 
-        # stdin / stdout redirects
-        if '>' in user_input:
+    rc = os.fork()
 
-            user_input = list(map(str.strip, user_input.rsplit('>')))
-            file_name  = user_input[1]
-            user_input = user_input[0]
-
-            os.close(1)
-            sys.stdout = open(file_name, 'w')
-            fd = sys.stdout.fileno()
-            os.set_inheritable(fd, True)
-        
-        elif '<' in user_input:
-
-            user_input = list(map(str.strip, user_input.split('<')))
-            file_name  = user_input[1]
-            user_input = user_input[0]
-
-            os.close(0)
-            sys.stdin = open(file_name, 'r')
-            fd = sys.stdin.fileno()
-            os.set_inheritable(fd, True)
-
-            user_input = user_input + ' ' + sys.stdin.read()
-            print(user_input)
-
-        argv = user_input.split()
-        if len(argv) == 0:
-            exit()
-        os.execvp(argv[0], argv)
+    if rc < 0:
+        sys.exit(1)
+    elif rc == 0:
+        execute_cmd_sq(raw)
     else:
         os.wait()
+    
